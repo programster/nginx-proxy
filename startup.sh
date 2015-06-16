16932beae3ba5dfb58a32873dc0a751de7fdb971
programster <sdpagent@gmail.com>
2015-06-16 14:28:58 +0000

    removing forego and calling the processes manually

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
-forego start -r
+# Run dockergen
+docker-gen -watch -only-exposed -notify "nginx -s reload" /app/nginx.tmpl /etc/nginx/conf.d/default.conf
+
+# Start the nginx service
+service nginx restart
 
 # Start the cron service in the foreground
 # We dont run apache in the FG, so that we can restart apache without container
